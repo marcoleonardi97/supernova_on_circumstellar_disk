67670a03b5c1215be59949b3a8fcf942d343644e
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2024-11-27 11:30:39 +0100

    Create supernova.py

---
+++ supernova.py
@@ -0,0 +1,12 @@
+from amuse.units import units
+
+def inject_supernova_energy(gas_particles, 
+                            explosion_energy=1.0e+51|units.erg,
+                            exploding_region=10|units.RSun):
+    inner = gas_particles.select(
+        lambda pos: pos.length_squared() < exploding_region**2,
+        ["position"])
+    print(len(inner), "innermost particles selected.")
+    print("Adding", explosion_energy / inner.total_mass(), "of supernova " \
+        "(specific internal) energy to each of the n=", len(inner), "SPH particles.")
+    inner.u += explosion_energy / inner.total_mass()
