3cbf2973d192871a34003d82849a40761e6563a5
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2024-11-27 14:31:03 +0100

    Update supernova.py

--- supernova.py
+++ supernova.py
@@ -1,6 +1,6 @@
 from amuse.units import units
 
-def inject_supernova_energy(gas_particles, 
+def inject_supernova_energy(gas_particles, heavy_elements_mass,
                             explosion_energy=1.0e+51|units.erg,
                             exploding_region=10|units.RSun):
     inner = gas_particles.select(
@@ -10,3 +10,4 @@ def inject_supernova_energy(gas_particles,
     print("Adding", explosion_energy / inner.total_mass(), "of supernova " \
         "(specific internal) energy to each of the n=", len(inner), "SPH particles.")
     inner.u += explosion_energy / inner.total_mass()
+    inner.metallicity += heavy_elements_mass / inner.total_mass()
