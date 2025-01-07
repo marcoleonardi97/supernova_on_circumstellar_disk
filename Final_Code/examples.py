63bf7c154394a79a67761161d757c7c9b3c49298
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2025-01-07 20:30:58 +0100

    Create examples.py

---
+++ Final_Code/examples.py
@@ -0,0 +1,43 @@
+from binary_disk import BinaryDisk
+
+tend = 10 | units.yr
+
+system = BinaryDisk(rin = 4 | units.au, rout = 5 | units.au, components = "all") # Cool ring figure
+system.plot_system(show=True)
+system.evolve(tend)
+
+system = BinaryDisk(rin = 0.5 | units.au, rout = 1 | units.au, components = "all") # Small disk
+system.plot_system(show=True)
+system.evolve(tend)
+
+system = BinaryDisk(rin = 1 | units.au, rout = 10 | units.au, components = "disk") # Disk only without the binary pair
+system.plot_system(show=True)
+system.evolve(tend)
+
+system = BinaryDisk(rin = 4 | units.au, rout = 5 | units.au, components = "stars") # Binary orbit, no disk
+system.plot_system(show=True)
+system.evolve(tend)
+
+# All of these simulations will fill your directory with images called "disk{time:.3f}.png", run this code to glue them together in a gif:
+
+import glob
+import os
+import imageio.v2 as imageio
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+# Make sure you don't have other .png files in your directory before running this code.
+files = glob.glob("*.png") # This will select all of your pngs
+
+numbers = []
+for f in files:
+    numbers.append(float(f[5:10]))
+ff = [y for _,y in sorted(zip(numbers, files))]
+
+with imageio.get_writer("15au_10years_5disk.gif", mode='I', duration=0.1) as writer:
+    for frame in ff:
+        image = imageio.imread(frame)
+        writer.append_data(image)
+
+# Clean up the directory 
+for frame in ff:
+    os.remove(frame)
