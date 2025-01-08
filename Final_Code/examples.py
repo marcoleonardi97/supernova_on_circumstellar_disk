cb2546fb73a4147fa699747c4e46da7ff979f2f4
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2025-01-08 11:56:10 +0100

    Update examples.py

--- Final_Code/examples.py
+++ Final_Code/examples.py
@@ -7,10 +7,6 @@ from binary_disk import BinaryDisk
 
 tend = 10 | units.yr
 
-system = BinaryDisk(rin = 4 | units.au, rout = 5 | units.au, components = "all") # Cool ring figure
-system.plot_system(show=True)
-system.evolve(tend, plot=True, verbose=False)
-
 system = BinaryDisk(rin = 0.5 | units.au, rout = 1 | units.au, components = "all") # Small disk
 system.plot_system(show=True)
 system.evolve(tend, plot=True, verbose=False)
@@ -22,6 +18,24 @@ system.evolve(tend, plot=True, verbose=False)
 system = BinaryDisk(rin = 4 | units.au, rout = 5 | units.au, components = "stars") # Binary orbit, no disk
 system.plot_system(show=True)
 system.evolve(tend, plot=True, verbose=False)
+
+
+#------- How to export and import existing systems ---------#
+
+# You can save a system using the class function save_particles(filename, memory=default, ow(overwrite)=False)
+system = BinaryDisk(rin = 1 | units.au, rout = 5 | units.au, semimaj = 15 | units.au, components="all")
+system.evolve(tend, plot=False, verbose=False)
+system.save_particles("test") # This will output the system.all_particles into an hdf5 file in your directory.
+
+# To reopen a saved system as a BinaryDisk class, you can call the parameter from_set at initialisation:
+saved_system = read_set_from_file('test.hdf5', 'amuse', close_file=True)
+new_system = BinaryDisk(from_set = saved_system)
+new_system.plot_system(show=True)
+
+
+
+
+
 
 # All of these simulations will fill your directory with images called "disk{time:.3f}.png", run this code to glue them together in a gif:
 
