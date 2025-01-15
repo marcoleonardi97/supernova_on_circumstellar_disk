00cdaf4aa2d16a491b5b8627221a88c79e245d4c
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2025-01-15 10:20:50 +0100

    Update supernova.py

--- Final_Code/supernova.py
+++ Final_Code/supernova.py
@@ -1,5 +1,3 @@
-# i hope this is the final version but maybe i copied the wrong one, careful
-
 import os
 import numpy
 from amuse.community.fi.interface import Fi
@@ -22,7 +20,6 @@ from mpl_toolkits.mplot3d import Axes3D
 
 os.environ["OMPI_MCA_rmaps_base_oversubscribe"]="true"
 options.GlobalOptions.instance().override_value_for_option("polling_interval_in_milliseconds", 10)
-
 class Supernova(object):
     def __init__(self, pickle=None, mass=10|units.MSun, nparticles=1000, external_object=None):
         self.pickle = pickle
@@ -35,7 +32,7 @@ class Supernova(object):
         if pickle is None:
             print("Creating initial conditions from a EVTwin stellar evolution model...")
             out_pickle_file = os.path.join(get_path_to_results(), 
-                                   "test.pkl") # You can change this name if you want
+                                   "test.pkl")
             stellar_evolution = EVtwin(redirection="none")  # Using EVtwin instead of MESA
             stars = Particles(1)
             stars.mass = 10 | units.MSun  # Massive star setup for supernova
@@ -47,12 +44,8 @@ class Supernova(object):
                 stellar_evolution.particles[0].mass
             )
         
-            while stellar_evolution.model_time < time:  
+            while stellar_evolution.model_time < 1 | units.yr:  
                 stellar_evolution.evolve_model()
-                if stellar_evolution.model_time >= tt:
-                    tt += 1 | units.yr
-                    print("Star:", stellar_evolution.particles[0].stellar_type, stellar_evolution.model_time.in_(units.Myr))
-                    #print(stellar_evolution.particles[0].luminosity)
             
             print("Evolved star to", stellar_evolution.particles[0].age)
             print("Radius:", stellar_evolution.particles[0].radius)
@@ -71,22 +64,22 @@ class Supernova(object):
         #        base_grid_options = dict(type = "glass", target_rms = 0.01),
         with_core_particle=True,
         target_core_mass = 1.4|units.MSun)
-        print("model=", model)
         self.core, self.gas_without_core, self.core_radius = \
                 model.core_particle, model.gas_particles, model.core_radius
         print("Created", len(self.gas_without_core),
                "SPH particles and one 'core-particle':\n", self.core)
         print("Setting gravitational smoothing to:", self.core_radius.in_(units.km))
 
-        self.core.position = (0, 0, 0) | units.au
-        self.gas_without_core.h_smooth = 0.6 | units.AU
-        self._explode() 
+        self.core.position = (0,0,0) | units.au
+        self.gas_without_core.h_smooth = 2 | units.AU
+        self.gas_without_core.name = "nova"
+        self.explode()
 
         self.converter = nbody_system.nbody_to_si(self.mass, self.core_radius)
         self.hydro = Fi(self.converter,mode='openmp',redirection='none')
         self.hydro.parameters.epsilon_squared = self.core_radius**2
         self.hydro.parameters.timestep = 1. | units.hour
-        #self.hydro.parameters.n_smooth_tol = 0.1
+        self.hydro.parameters.n_smooth_tol = 0.1
         self.hydro.parameters.isothermal_flag = False
         self.hydro.parameters.integrate_entropy_flag = True
         self.hydro.gas_particles.add_particles(self.gas_without_core)
@@ -96,57 +89,68 @@ class Supernova(object):
         self.particles.add_particles(self.gas_without_core)
         self.gas_particles.add_particles(self.gas_without_core)
 
-        self.external_code = Fi(self.converter, mode='openmp', redirection='none')
-        self.external_code.particles.add_particles(self.external_object)
+        if self.external_object is not None:
+            self.external_code = Fi(self.converter, mode='openmp', redirection='none')
+            self.external_code.parameters.use_hydro_flag = True
+            self.external_code.parameters.radiation_flag = False
+            self.external_code.parameters.self_gravity_flag = True
+            self.external_code.parameters.gamma = 1.
+            self.external_code.parameters.isothermal_flag = True
+            self.external_code.parameters.integrate_entropy_flag = False
+            self.external_code.parameters.timestep = 0.1 | units.yr
+            self.external_code.particles.add_particles(self.external_object)
 
-    def _explode(self, 
-        
-                        explosion_energy=1.0e+51|units.erg,
-                        exploding_region=10|units.RSun):
-
-        """
-        Supernova energy injection function:
-        Injects 10e51 ergs of energy divided into all the particles inside the exploding region squared.
-        """
-        inner = self.gas_without_core.select(
-            lambda pos: pos.length_squared() < exploding_region**2,
-            ["position"])
+    def explode(self, explosion_energy=1.0e+51|units.erg, exploding_region=1|units.RSun):
+        inner = self.gas_without_core.select(lambda pos: pos.length_squared() < exploding_region**2, ["position"])
         print(len(inner), "innermost particles selected.")
         print("Adding", explosion_energy / inner.total_mass(), "of supernova " \
             "(specific internal) energy to each of the n=", len(inner), "SPH particles.")
         inner.u += explosion_energy / inner.total_mass()
 
-    def _bridge(self):
+    def bridge(self):
         bridged = bridge.Bridge(use_threading=False, method=SPLIT_4TH_S_M4)
         bridged.add_system(self.hydro, (self.external_code,))
-        bridged.add_system(self.external_code, (self.hydro,)) # probably don't need this, but also probably doesn't change anything unfortunately
+        bridged.add_system(self.external_code, (self.hydro,))
         bridged.timestep = self.hydro.parameters.timestep
         return bridged
-        
 
     def evolve(self, tend, plot=False, plot3d = False, verbose=False):
-        """
-        Evolve the system:
-        @tend: time - this will be added and remembered in the system_time. 
-                      if you evolve the same system twice it will restart 
-                      from the last timestep.
-        @plot: bool - This will save a .png for each timestep of the evolution
-        @plot3d: bool - will save a .png of a 3d plot for each timestep
-        @verbose: bool - will print average position of particles to see what's moving.
-        """
         time = 0 | units.day
         
         if self.external_object is None:
             code = self.hydro
         else:
-            code = self._bridge()
+            code = self.bridge()
             
         while time < tend:
+            
             code.evolve_model(time)
             print(f"Evolution: {time.in_(tend.unit)}")
             time += self.hydro.parameters.timestep
-            self.external_code.particles.new_channel_to(self.external_object).copy()
 
+            # i give up Bridge won't transfer energy so i will (still particles don't move accordingly though)
+            if self.external_object is not None:
+                f = 0.5
+                for particle in self.external_object:
+                    closest = sn.gas_without_core.find_closest_particle_to(*particle.position)
+                    if (closest.position - particle.position).length().in_(units.au) < 1 | units.au:
+                        particle.u += f * closest.u
+                        particle.vx += f  * closest.vx
+                        particle.vy += f * closest.vy
+                        particle.vy += f * closest.vz
+                        closest.u -= f * particle.u
+                        closest.vx -= f * particle.vx
+                        closest.vy -= f * particle.vy
+                        closest.vz -= f * particle.vz
+
+                
+                
+                self.external_code.particles.new_channel_to(self.external_object).copy()
+                self.hydro.particles.new_channel_to(self.gas_without_core).copy()
+                self.gas_without_core.new_channel_to(self.external_object).copy_attributes(["u", "u", "vx", "vy",
+                                                                                           "vz", "mass"])
+                
+                
             if plot:
                 self.plot_system(show=False, save=True, time=time)
     
@@ -159,13 +163,42 @@ class Supernova(object):
                 print("Local self.gas_particles (gas): ", np.mean(self.gas_particles.x.in_(units.au)))
                  
         print("Done.")
+
+    def evolve_external_only(self, tend, plot=False, plot3d = False, verbose=False):
+        time = 0 | units.yr
+        
+        if self.external_object is None:
+            print("No external object assigned")
+            return
+            
+        while time < tend:
+            #self.hydro.evolve_model(time)
+            self.external_code.evolve_model(time)
+            print(f"Evolution: {time.in_(tend.unit)}")
+            time += self.external_code.parameters.timestep
+
+            if plot:
+                plt.figure()
+                scatter(self.external_code.particles.x.in_(units.au), self.external_code.particles.y.in_(units.au))
+                plt.xlim(-10,10)
+                plt.ylim(-10,10)
+                plt.title(f"{time.number:.3f}{time.unit}")
+                plt.savefig(f"disk_{time.number:.3f}.png")
+                
+            if plot3d:
+                self.plot3d(show=False, save=True, time=time)
+    
+            if verbose:
+                print(f"External object mean energy: {np.mean(self.external_object.u)}")
+                 
+        print("Done.")
     
     def plot_system(self, show=False, save=False, time=None):
         plt.figure()
         if self.external_object is None:
             l = 10
         else: 
-            l = max(self.external_object.x.in_(units.au)).number
+            l = max(self.external_object.x.in_(units.au)).number * 2
         plt.xlim(-l,l)
         plt.ylim(-l,l)
     
@@ -173,9 +206,9 @@ class Supernova(object):
         scatter(self.hydro.dm_particles.x.in_(units.au), self.hydro.dm_particles.y.in_(units.au), marker='*', c='yellow', label="Core")
         if self.external_object is not None:
             try:
-                scatter(self.external_object.x.in_(units.au), self.external_object.y.in_(units.au), c='orange', alpha=0.1, label=self.external_object.name[0])
+                scatter(self.external_code.particles.x.in_(units.au), self.external_code.particles.y.in_(units.au), c='orange', alpha=0.1, label=self.external_object.name[0])
             except:
-                 scatter(self.external_object.x.in_(units.au), self.external_object.y.in_(units.au), c='orange', alpha=0.1, label="External Object")
+                scatter(self.external_code.particles.x.in_(units.au), self.external_code.particles.y.in_(units.au), c='orange', alpha=0.1, label="External Object")
          
         plt.legend(loc='upper right')
         
@@ -197,16 +230,14 @@ class Supernova(object):
         
         # Create a 3D plot
         fig = plt.figure()
-        if self.external_object is None:
-            l = 10
-        else: 
-            l = max(self.external_object.x.in_(units.au)).number
+        l = 10
         ax = fig.add_subplot(111, projection='3d')
     
         ax.set_xlim(-l,l)
         ax.set_ylim(-l,l)
         ax.set_zlim(-l,l)
     
+        # Scatter plot of the particles
         ax.scatter(x.number, y.number, z.number, s=1, c='blue', marker='o', label="SN")  # Adjust size and color as needed
         if self.external_object is not None:
             ex = self.external_object.x.in_(units.au)
@@ -218,6 +249,7 @@ class Supernova(object):
                  ax.scatter(ex.number, ey.number, ez.number, c='orange', alpha=0.1, label="External Object")
      
         plt.legend(loc='upper right')
+        # Set labels
         ax.set_xlabel('X au')
         ax.set_ylabel('Y au')
         ax.set_zlabel('Z au')
@@ -230,7 +262,10 @@ class Supernova(object):
             plt.close()
 
     def __str__(self):
-        print(f"Supernova object of {self.nparticles} particles.")
+        print("Supernova class WIP")
+        return ""
+    
+
         if self.external_object is not None:
           print(f"The supernova is hitting a body at position:")
           print(self.external.object.position)
