ef0c6f34808569a0d3d0077d3d904ffeac2ef121
Marco Leonardi <32566105+marcoleonardi97@users.noreply.github.com>
2025-01-26 21:13:23 +0100

    Update main_part_1.py

--- Final_Code/main_part_1.py
+++ Final_Code/main_part_1.py
@@ -5,14 +5,12 @@ from binary_disk import BinaryDisk  # %run binary_disk.ipynb on jupyter lab
 # To evolve this system with bridge you can simply call system.evolve():
 # To check the particles and model_time of the system you can print the class: print(system)
 
-# This is the real part 1 code, run for however long you want.
-system = BinaryDisk(rin = 1 | units.au, rout = 5 | units.au, semimaj = 25 | units.au, components="all")
+# This is an example of the real part 1 code, run for however long you want.
+# The 4 simulations to present are saved in the backups folder (evolved for 50 years)
+system = BinaryDisk(rin = 1 | units.au, rout = 5 | units.au, semimaj = 25 | units.au, density=10e-10 | (units.g / units.cm**3) components="all")
 system.plot_system(show=True) # This will show a snapshot of the system
 t_100 | units.yr
-system.evolve(t_end, plot=True, verbose=False)
-
-system.save_particles('ev_100')
-print(system) # Save the parameters manually if you need them, it's not really worth the effort to implement an automation for this i don't think
+system.evolve(t_end, plot=True, backup=True)
 
 
 # Animation - this method only works if you don't have other pngs in your directory.
